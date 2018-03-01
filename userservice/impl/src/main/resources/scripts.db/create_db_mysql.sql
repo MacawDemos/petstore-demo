3a9a077833f27b870cd5265ef7994ce72a45dbbb
Ram Datla <34409202+macawdemo@users.noreply.github.com>
2018-03-01 11:08:23 +0000

    Update create_db_mysql.sql

--- userservice/impl/src/main/resources/scripts.db/create_db_mysql.sql
+++ userservice/impl/src/main/resources/scripts.db/create_db_mysql.sql
@@ -25,7 +25,7 @@ create table ACCOUNT (
     email varchar(80) not null,
     firstname varchar(80) not null,
     lastname varchar(80) not null,
-    status varchar(2)  null,
+    status varchar(5)  null,
     addr1 varchar(80) not null,
     addr2 varchar(40) null,
     city varchar(80) not  null,
